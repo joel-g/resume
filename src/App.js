b91264a5ea528dfad40b5c9bc2975e853e24ec64
Joel Guerra <joelaguerra1@gmail.com>
2017-08-06 17:56:06 -0700

    set new header

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>joel guerra</h1>
+          <p className="App-intro">
+            joel makes things
+          </p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
